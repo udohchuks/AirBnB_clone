4cdd1a90b0dfb3050f79cb7419e68aed748ca5d9
chukwucyprian <chukwucyprian262@gmail.com>
2023-08-09 22:18:24 +0000

    more

--- models/base_model.py
+++ models/base_model.py
@@ -16,20 +16,24 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """
-            Constructor for BaseModel
-        """
+        """Instantiation of base model class
+            Args: args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+            Attributes:
+                id: unique id generated
+                created_at: creation date
+                updated_at: updated date
+         """
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -54,25 +58,3 @@ class BaseModel:
 
         return new_dict
 
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
