7630c4ca3e13e676ff214997317954608e0a5710
udohchuks <urichard132@gmail.com>
2023-08-13 20:24:47 +0100

    fix pycodestyle

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,43 +13,43 @@ class TestHBNBCommand(unittest.TestCase):
         self.console = HBNBCommand()
 
     def test_do_create(self):
-       """Test Create"""
-       with patch("sys.stdout", new=StringIO()) as output:
-           input = "create"
-           expected = "** class name missing **"
-           HBNBCommand().onecmd(input)
-           self.assertEqual(expected, output.getvalue().strip())
+        """Test Create"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            input = "create"
+            expected = "** class name missing **"
+            HBNBCommand().onecmd(input)
+            self.assertEqual(expected, output.getvalue().strip())
 
     def test_do_show(self):
-       """Test Show"""
-       with patch("sys.stdout", new=StringIO()) as output:
-           input = "create"
-           expected = "** class name missing **"
-           HBNBCommand().onecmd(input)
-           self.assertEqual(expected, output.getvalue().strip())
+        """Test Show"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            input = "create"
+            expected = "** class name missing **"
+            HBNBCommand().onecmd(input)
+            self.assertEqual(expected, output.getvalue().strip())
 
     def test_do_all(self):
-       """Test all"""
-       with patch("sys.stdout", new=StringIO()) as output:
-           input = "all"
-           HBNBCommand().onecmd(input)
-           self.assertIsNotNone(output.getvalue().strip())
- 
+        """Test all"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            input = "all"
+            HBNBCommand().onecmd(input)
+            self.assertIsNotNone(output.getvalue().strip())
+
     def test_do_destroy(self):
-       """Test Destroy"""
-       with patch("sys.stdout", new=StringIO()) as output:
-           input = "destroy"
-           expected = "** class name missing **"
-           HBNBCommand().onecmd(input)
-           self.assertEqual(expected, output.getvalue().strip())
+        """Test Destroy"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            input = "destroy"
+            expected = "** class name missing **"
+            HBNBCommand().onecmd(input)
+            self.assertEqual(expected, output.getvalue().strip())
 
     def test_do_update(self):
-       """Test update"""
-       with patch("sys.stdout", new=StringIO()) as output:
-           input = "update"
-           expected = "** class name missing **"
-           HBNBCommand().onecmd(input)
-           self.assertEqual(expected, output.getvalue().strip())
+        """Test update"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            input = "update"
+            expected = "** class name missing **"
+            HBNBCommand().onecmd(input)
+            self.assertEqual(expected, output.getvalue().strip())
 
 
 if __name__ == '__main__':
